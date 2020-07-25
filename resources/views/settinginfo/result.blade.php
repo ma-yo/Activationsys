ad675cdcd16abfd63cc039b3b078d79af6208b67
昌史 吉田 <m.yoshida.1977@googlemail.com>
2020-07-26 06:09:32 +0900

    新規ファイル追加

---
+++ resources/views/settinginfo/result.blade.php
@@ -0,0 +1,53 @@
+@extends('layout.common')
+@section('title', 'アクティベーションシステム - 設定値編集済')
+@section('css')
+<link href="css/settinginfo-result.css?{{$commons['systemdate']}}" rel="stylesheet" type="text/css">
+@endsection
+
+@section('js')
+<script type="text/javascript" src="js/settinginfo-result.js?{{$commons['systemdate']}}"></script>
+@endsection
+
+@section('content')
+
+<div id="settinginfo-result">
+    <p class="h2 text-primary">設定情報を変更しました。</p>
+
+    <form id="menuback-form" class="form" action="/menu" method="get">
+        @csrf
+        <div class="form-group form-inline">
+            <span class="mr-auto"></span>
+            <button type="button" id="menuback-button" name="menuback-button" class="btn btn-secondary btn-md ml-2">メニューに戻る</button>
+        </div>
+    </form>
+
+    <div class="border border-primary rounded p-3 mb-3">
+        <table id="resultsettinginfo-table" class="table table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>プロパティ</th>
+                    <th>値</th>
+                    <th>登録日時</th>
+                    <th>更新日時</th>
+                </tr>
+            </thead>
+            @if(!empty($datas['settinginfos']))
+                <tbody>
+                    @foreach($datas['settinginfos'] as $settinginfo)
+                        <tr>
+                            <td><small>{{ $settinginfo->settingid }}</small></td>
+                            <td><small>{{ $settinginfo->description }}</small></td>
+                            <td><small>{{ $settinginfo->value1 }}</small></td>
+                            <td><small>{{ $settinginfo->created_at }}</small></td>
+                            <td><small>{{ $settinginfo->updated_at }}</small></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            @endif
+        </table>
+    </div>
+</div>
+
+
+@endsection
