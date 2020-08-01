7bb31c90e02e5cb476b4eb3d965ab6ea387fd463
昌史 吉田 <m.yoshida.1977@googlemail.com>
2020-08-01 10:10:56 +0900

    タイトルにテンプレートファイル文言が入っていたのを修正

--- resources/views/layout/common.blade.php
+++ resources/views/layout/common.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title')｜Blade Template file</title>
+    <title>@yield('title')</title>
 
     <link rel="stylesheet" href="css/local/bootstrap/bootstrap-4.3.1.min.css?{{$commons['systemdate']}}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
